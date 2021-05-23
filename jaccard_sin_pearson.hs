eefc88e40cdd7a53b35cdc228cbe8245d931abff
Theodor René Carlsen <theodor.rene@gmail.com>
2021-05-23 16:55:12 +0200

    chagnes

--- jaccard_sin_pearson.hs
+++ jaccard_sin_pearson.hs
@@ -1,8 +1,18 @@
 module Main where
+    import Data.List(transpose)
+
     main :: IO ()
     main = print "hello"
 
-    userrating   = [[1,0,3,0,0,5,0,0],[0,0,5,4,0,0,4,0],[2,4,0,1,2,0,3,0],[0,2,4,0,5,0,0,4],[1,0,3,0,3,0,0,2]] :: [[Double]]
+    userrating   = [[1,0,3,0,0,5,0,0],[0,0,5,4,0,0,4,0],[2,4,0,1,2,0,3,0],[0,2,4,0,5,0,0,4],[0,0,4,3,4,2,0,0], [1,0,3,0,3,0,0,2]] :: [[Double]]
+    columns = transpose userrating 
+
+    six = userrating !! 5
+
+    prettyp :: [[Double]] -> IO ()
+    prettyp = mapM_ print 
+
+    userratingBits = map valueToOne userrating
 
     valueToOne :: [Double] -> [Double]
     valueToOne = map (\x -> if x > 0 then 1 else 0) 
@@ -37,16 +47,18 @@ module Main where
     -- S_xy
     removeUnratedRecords a b = unzip . filter (uncurry (==)) $ zip a b
 
+    removeUnratedRecords2 a b = unzip . filter (\(x, y) -> x /=0.0 && y /= 0.0) $ zip a b
+
     pearson a b = weightedAverage / (meanSumLengthVector avg_a l1 * meanSumLengthVector avg_b l2)
         where weightedAverage = meanSumVectorMultiplication avg_a avg_b l1 l2
               normalize aveg = map (subtract aveg)
               avg_a = avg a
               avg_b = avg b
-              (l1,l2) = removeUnratedRecords a b
+              (l1,l2) = removeUnratedRecords2 a b
 
     avg l = sum l / fromIntegral (length l)
 
 
-    weightedAverage (vectors,sim) itemIndex = numerator / denominator
+    weightedAverage1 (vectors,sim) itemIndex = numerator / denominator
             where numerator = sum $ map (*(sim!!itemIndex)) (vectors !! itemIndex)
                   denominator = sum sim
