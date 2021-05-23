fd817d6757492e11405aaa7c63d3d3f9d8bb355a
Theodor René Carlsen <theodor.rene@gmail.com>
2021-05-23 13:49:20 +0200

    refactor and add comments

--- jaccard_sin_pearson.hs
+++ jaccard_sin_pearson.hs
@@ -2,27 +2,42 @@ module Main where
     main :: IO ()
     main = print "hello"
 
-
     
-    cosineSim a b = atimesb / (lengthVector a * lengthVector b)
-        where atimesb = sum $ zipWith (*) a b
+    -- Σ(A-meanA)*(B-meanB)
+    meanSumVectorMultiplication avgA avgB a b = sum $ zipWith (*) normalizedA normalizedB
+        where subtractMean average vector = map (subtract average) vector
+              normalizedA = subtractMean avgA a
+              normalizedB = subtractMean avgB b
 
-    lengthVector = sqrt . sum . map (^2)
+    -- ΣA*B
+    vectorSumMultiplication = meanSumVectorMultiplication 0 0 
+    
+    cosineSim a b = vectorSumMultiplication a b / (sumLengthVector a * sumLengthVector b)
+
+
+    -- √ΣA²
+    sumLengthVector = sqrt . sum . map (^2)
+
+    -- √Σ(A-meanA)²
+    meanSumLengthVector average = sumLengthVector . map (subtract average)
 
     jaccard a b = nrSameItems / nrAllItems
-        where nrSameItems = sum  $ zipWith (\x y -> boolToInt (x == y && x /= 0)) a b
-              nrAllItems = sum $ zipWith (\x y -> boolToInt (x == 0 && y == 0)) a b
-              boolToInt bool = if bool then 1 else 0
+        where nrSameItems = sum  $ zipWith (\x y -> boolToNum (x == y && x /= 0)) a b
+              nrAllItems = sum $ zipWith (\x y -> boolToNum (x == 0 && y == 0)) a b
 
-    pearson1 a b = unzip . filter (uncurry (==)) $ zip a b
+    boolToNum True = 1
+    boolToNum False = 0
 
-    pearson a b = weightedAverage / (weightedLength avg_a l1 * weightedLength avg_b l2)
-        where weightedAverage = sum $ zipWith (*) (normalize avg_a l1) (normalize avg_b l2)
+
+    -- S_xy
+    removeUnratedRecords a b = unzip . filter (uncurry (==)) $ zip a b
+
+    pearson a b = weightedAverage / (meanSumLengthVector avg_a l1 * meanSumLengthVector avg_b l2)
+        where weightedAverage = meanSumVectorMultiplication avg_a avg_b l1 l2
               normalize aveg = map (subtract aveg)
               avg_a = avg a
               avg_b = avg b
-              weightedLength average list = lengthVector . map (subtract average) $ list
-              (l1,l2) = pearson1 a b
+              (l1,l2) = removeUnratedRecords a b
 
     avg l = sum l / fromIntegral (length l)
 
